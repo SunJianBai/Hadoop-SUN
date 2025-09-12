701a6875ceaef145fb7faf3fb783b65d43d2160d
梓仁沐白 <2274540432@qq.com>
2025-09-12 16:50:26 +0800

    上传文件到hdfs

--- java_src/InvertedMapReduce/src/main/java/com/test/Driver.java
+++ java_src/InvertedMapReduce/src/main/java/com/test/Driver.java
@@ -13,11 +13,11 @@ public class Driver {
     public static void main(String[] args) throws ClassNotFoundException, IOException,InterruptedException {
         Configuration conf = new Configuration();
         //hdfs 主NameNode通信地址
-        conf.set("fs.defaultFS","hdfs://hadoop081:9000");
+        conf.set("fs.defaultFS","hdfs://master:9000");
         //yarn 主resourcemanager通信地址
-        conf.set("yarn.resourcemanager.hostname","hadoop081");
+        conf.set("yarn.resourcemanager.hostname","master");
         //zookeeper集群，连接到HMaster
-        conf.set("hbase.zookeeper.quorum","hadoop081,hadoop082,hadoop083");
+        conf.set("hbase.zookeeper.quorum","master,worker1,worker2");
 
         Job job = Job.getInstance(conf);
         job.setJarByClass(Driver.class);
